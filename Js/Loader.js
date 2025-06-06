be7e68ef57d4bfb2cdc4fa249417738e316aeb61
SamuelGB <samuelguardiabautista@gmail.com>
2025-06-06 18:30:30 -0500

    Update Loader.js

--- Js/Loader.js
+++ Js/Loader.js
@@ -1,18 +1,9 @@
-// Loader.js
-function mostrarLoader() {
-  const loader = document.createElement("div");
-  loader.id = "loader";
-  loader.className = "loader-overlay";
-  loader.innerHTML = `
-    <div>
-      <div class="spinner"></div>
-      <p style="color:white; text-align:center; margin-top:10px;">Cargando...</p>
-    </div>
-  `;
-  document.body.appendChild(loader);
-}
-
-function ocultarLoader() {
+window.mostrarLoader = function () {
   const loader = document.getElementById("loader");
-  if (loader) loader.remove();
-}
+  if (loader) loader.style.display = "flex";
+};
+
+window.ocultarLoader = function () {
+  const loader = document.getElementById("loader");
+  if (loader) loader.style.display = "none";
+};
