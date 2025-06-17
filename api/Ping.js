e41b197fa1b6d038d9aa20498d1e1765af96b5ca
SamuelGB <samuelguardiabautista@gmail.com>
2025-06-17 09:29:51 -0500

    Update Ping.js

--- api/Ping.js
+++ api/Ping.js
@@ -1,15 +1,20 @@
 // /api/ping.js
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
+  if (req.method !== "GET" && req.method !== "HEAD") {
     return res.status(405).json({ error: "Método no permitido" });
   }
-
+  if (req.method === "HEAD") {
+    return res.status(200).send('OK');
+  }
   const supabaseUrl = 'https://bsrtuievwjtzwejuxqee.supabase.co';
   const supabaseKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzcnR1aWV2d2p0endlanV4cWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTEyNjYsImV4cCI6MjA2NDEyNzI2Nn0.A9tCs-Zi-7jw5LUFs7ViIR2vHb9tNMj6c7YeeNOdmWI"
   const tableName = 'ping_log';
-
-  try {
+  
+  const fechaLocal = new Date().toLocaleString("sv-SE", {
+    timeZone: "America/Lima"
+  }).replace(" ", "T");
+   try {
     const response = await fetch(`${supabaseUrl}/rest/v1/${tableName}`, {
       method: 'POST',
       headers: {
@@ -17,7 +22,10 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${supabaseKey}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ origen: 'uptimerobot' })
+      body: JSON.stringify({
+        origen: 'uptimerobot',
+        fecha_local: fechaLocal
+      })
     });
 
     if (!response.ok) {
