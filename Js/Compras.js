5352271172102f9524e25f6b349a24dbc5ec877c
SamuelGB <samuelguardiabautista@gmail.com>
2025-07-02 09:26:04 -0500

    Update Compras.js

--- Js/Compras.js
+++ Js/Compras.js
@@ -70,7 +70,9 @@ async function mostrarDetalleCompras(codigo) {
     <tbody>`;
 
   for (const e of entregas) {
-    const fechaFormateada = new Date(e.fecha_entrega).toLocaleDateString("es-PE");
+    const fechaFormateada = !e.fecha_entrega
+      ? "Pendiente"
+      : new Date(e.fecha_entrega).toLocaleDateString("es-PE");
     tablaHtml += `<tr>
       <td class="td-mini">${e.nro_oc}</td>
       <td class="td-mini">${formatearNumero(e.cantidad)}</td>
